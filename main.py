be43ef592463b54fc516b60a972511be957df3a6
Ethan Christensen <ethan.christensen@fiserv.com>
2023-11-09 13:09:17 -0600

    yep

--- main.py
+++ main.py
@@ -1,53 +1,99 @@
+import re
+import os
 import pandas as pd
 
+class IterativeShell:
 
-class VersionControlledDataframe:
     def __init__(self):
         self.commands = {
             "hello": self.hello,
-            "load": self.load,
+            "data": self.data,
+            "ik": self.ik,
             "view": self.view,
             "filter": self.filter_where,
             "rollback": self.rollback,
-            "drop": self.drop
+            "drop": self.drop,
+            "score": self.score,
+            "columns": self.columns,
         }
 
         self.data = {
+            "ik": {
+                "clover": "clv",
+                "clvr": "clv",
+                "tck": "telecheck",
+                "ecommerce": "ecom",
+                "chargebacks": "chargeback",
+                "qsr": "quick_service",
+                "act": "mag"
+            },
             "frames": {
+                "base": None,
                 "dataframe": None,
                 "_dataframe": None,
                 "__dataframe": None,
-            }
+            },
+            "memory_paths": {
+                "dataframe": "./memory/a.pkl",
+                "_dataframe": "./memory/aa.pkl",
+                "__dataframe": "./memory/aaa.pkl",
+            },
+            "has_updated": False,
         }
-
+    
     def hello(self):
-        response = "Hello World!"
-        return response
-    
-    def load(self):
-        file_path = input(" : enter file path to csv: ")
-        try:
-            dataframe = pd.read_csv(file_path, low_memory=False)
-            self.data["frames"]["dataframe"] = dataframe
-            response = "data successfully loaded!"
-            return response
-        except:
-            response = "error. data not loaded!"
-            return response
-    
+        return "Hello World!"
+
+    def _save_frames_to_memory(self):
+        if self.data.get("has_updated"):
+            print(" : saving progress...")
+
+            frames = list(self.data.get("frames"))[1:]
+            base_mem_path = "memory"
+            memory_paths = list(self.data.get("memory_paths").values())
+
+            for frame_idx, frame in enumerate(frames):
+                if (tmp := self.data.get("frames").get(frame)) is not None:
+                    tmp.to_pickle(memory_paths[frame_idx])
+                else:
+                    if os.path.exists(memory_paths[frame_idx]):
+                        os.remove(memory_paths[frame_idx])
+
+    def _check_and_load(self):
+        base_mem_path = "./memory"
+        memory_files = os.listdir(base_mem_path)
+        if len(memory_files) != 0:
+            frame_idxs = list(self.data.get("frames"))[1:]
+            print(" : loading previous state...")
+            for mem_idx, mem_file in enumerate(memory_files):
+                self.data["frames"][frame_idxs[mem_idx]] = pd.read_pickle(f"{base_mem_path}/{mem_file}")
+            print(" : previous state successfully loaded!")
+
     def set_frames(self, df1, df2, df3):
         self.data.get("frames")["dataframe"] = df1
         self.data.get("frames")["_dataframe"] = df2
         self.data.get("frames")["__dataframe"] = df3
 
-    def is_loaded(self):
+    def is_loaded(self, *args):
         return self.data.get("frames").get("dataframe") is not None
 
-    def view(self):
+    def view(self, *args):
         if not self.is_loaded(): return "no data loaded!"
-        print(self.data.get("frames").get("dataframe").head())
+        if args and args[0].strip() != '':
+            filter_cols = args[0]
+            if filter_cols[0] != "[" or filter_cols[-1] != "]":
+                return "arg of 'view' must look like '[col1, col2, col3, ...]' to get a filtered view"
+            valid_columns = self.data.get("frames").get("dataframe").columns.values.tolist()
+            filter_cols = [_.strip() for _ in filter_cols[1:-1].split(",")]
+            filter_cols = [col for col in filter_cols if col in valid_columns]
+            print(self.data.get("frames").get("dataframe").head()[filter_cols])
+        else:
+            print(self.data.get("frames").get("dataframe").head())
 
     def _bubble(self, bubbleframe):
+        if not self.data.get("has_updated"):
+            self.data["has_updated"] = True
+
         v1 = self.data.get("frames").get("dataframe")
         v2 = self.data.get("frames").get("_dataframe")
 
@@ -57,7 +103,7 @@ class VersionControlledDataframe:
             v2
         )
 
-    def filter_where(self):
+    def filter_where(self, *args):
         if not self.is_loaded(): return "no data loaded!"
         current_dataframe = self.data.get("frames").get("dataframe").copy()
         valid_columns = self.data.get("frames").get("dataframe")
@@ -70,8 +116,10 @@ class VersionControlledDataframe:
         if column_is not in valid_filters: return "not a valid filter!"
         filter_value = input(" : enter value to filter by: ")
 
-        if filter_value.isdigit():
+        if (filter_value.isdigit()) or (filter_value[0] == '-' and filter_value[1:].isdigit()):
             filter_value = int(filter_value)
+        if filter_value in ["True", "False"]:
+            filter_value = filter_value == "True"
         
         try:
             if column_is == "==":
@@ -94,7 +142,12 @@ class VersionControlledDataframe:
 
         return "filter successfully applied!"
 
-    def rollback(self):
+    def rollback(self, *args):
+        if not self.is_loaded(): return "no data loaded, nothing to rollback!"
+
+        if not self.data.get("has_updated"):
+            self.data["has_updated"] = True
+
         v2 = self.data.get("frames").get("_dataframe")
         v3 = self.data.get("frames").get("__dataframe")
 
@@ -104,7 +157,9 @@ class VersionControlledDataframe:
             None
         )
 
-    def drop(self):
+        return "rollback successful!"
+
+    def drop(self, *args):
         if not self.is_loaded(): return "no data loaded!"
         valid_columns = self.data.get("frames").get("dataframe")
         drop_columns = input(" : enter columns to drop (, separated): ")
@@ -121,24 +176,40 @@ class VersionControlledDataframe:
 
         return "drop successfully applied!"
 
-    def shell(self):
-        while (query := input(":: ")) not in ["Q", "q", "quit"]:
-            _query = query.strip().lower()
-            _queries = [_.strip() for _ in _query.split("&")]
-            for _q in _queries:
-                if _q in self.commands.keys():
-                    try:
-                        response = self.commands[_q]()
-                    except KeyboardInterrupt:
-                        response = "command interrupted"
-                    if response:
-                        print(f" : {response}")
-                else:
-                    print(f" : '{_q}' is not a valid command")
-        else:
+    def columns(self, *args):
+        if not self.is_loaded(): return "no data loaded!"
+        return ", ".join(self.data.get("frames").get("dataframe").columns.values.tolist())
+
+    def enter(self):
+        self._check_and_load()
+
+        try:
+            while (query := input(":: ")) not in ["Q", "q", "quit"]:
+                _query = query.strip().lower()
+                _queries = [_.strip() for _ in _query.split("&")]
+                for _q in _queries:
+                    _q_parts = _q.split(" ")
+                    if _q_parts[0] in self.commands.keys():
+                        try:
+                            response = self.commands[_q_parts[0]](" ".join(_q_parts[1:]))
+                        except KeyboardInterrupt:
+                            response = "command interrupted"
+                        except Exception as error:
+                            response = f"command '{_q_parts[0]}' failed with error: '{error}'"
+
+                        if response:
+                            print(f" : {response}")
+                    else:
+                        print(f" : '{_q}' is not a valid command")
+            else:
+                self._save_frames_to_memory()
+                print(" : exiting")
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            self._save_frames_to_memory()
             print(" : exiting")
-        
+
 
 if __name__ == "__main__":
-    df = VersionControlledDataframe()
-    df.shell()
+    shell = IterativeShell()
+    shell.enter()
